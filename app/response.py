081914375a41e81bc40be225c363943f281d732f
kcrong <kcrong@kim82536.pe.kr>
2016-09-28 10:26:43 +0900

    response 에서 상태 코드에 따라 status값 변하게 함

--- app/response.py
+++ app/response.py
@@ -1,7 +1,14 @@
 from flask import jsonify
 
 
-def json_status(code=200, status='Success', data=None):
+def json_status(code=200, status=None, data=None):
+    if status is not None:
+        pass
+    elif code == 200:
+        status = 'Success'
+    else:
+        status = 'Failed'
+
     default = {
         'code': code,
         'status': status
