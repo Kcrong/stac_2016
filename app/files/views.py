91bf0e812b664e538b2a35b895fb83bdfdbd8e69
kcrong <kcrong@kim82536.pe.kr>
2016-09-29 13:44:07 +0900

    뷰에서 프로필 사진 추가/조회 기능 추가

--- app/files/views.py
+++ app/files/views.py
@@ -1,17 +1,27 @@
 from . import file_blueprint
-from flask import request
+from flask import request, send_file
 from ..user.manager import *
 from ..models import *
 
 
 @file_blueprint.route('/image', methods=['GET', 'POST'])
-def comment():
+def image():
+    profile_image = request.files['file']
     form = request.form
+
+    def get():
+        image = User.query.filter_by(userid=form['userid']).first().image
+        return send_file('../static/' + image)
 
     @login_required
     def post():
+        u = current_user()
+        u.add_profile(profile_image)
+        db.session.commit()
+
         return json_status()
 
     return {
+        'GET': get,
         'POST': post
     }[request.method]()
