ec2a311af20d9fb44cdac1739575caf33b0747ac
kcrong <kcrong@kim82536.pe.kr>
2016-09-29 14:11:08 +0900

    프로필 조회 기능 추가 파일 받아오는 것을 post 함수 내에서 받아옴

--- app/files/views.py
+++ app/files/views.py
@@ -2,24 +2,26 @@ from . import file_blueprint
 from flask import request, send_file
 from ..user.manager import *
 from ..models import *
+from config import PROFILE_IMAGE_PATH
 
 
 @file_blueprint.route('/image', methods=['GET', 'POST'])
 def image():
-    profile_image = request.files['file']
+    args = request.args
     form = request.form
 
     def get():
-        image = User.query.filter_by(userid=form['userid']).first().image
-        return send_file('../static/' + image)
+        image = User.query.filter_by(userid=args['userid']).first().image
+        return send_file(os.path.join(PROFILE_IMAGE_PATH, image))
 
     @login_required
     def post():
+        profile_image = request.files['file']
         u = current_user()
         u.add_profile(profile_image)
         db.session.commit()
 
-        return json_status()
+        return json_status(data={'image': u.image})
 
     return {
         'GET': get,
